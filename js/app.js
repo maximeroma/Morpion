6b115947ab245e78997e5b1bf59c3a57e5923e0e
romamaxime <roma.maxime1994@gmail.com>
2017-04-11 14:05:42 +0200

    add var bool and alert winner

--- js/app.js
+++ js/app.js
@@ -8,15 +8,37 @@ $(document).ready(function()
 					[1, 1, 1],
 					[1, 1, 1],	];
 
-
+	var bool = true; // bool devient false quand il y a un vainqueur
 	var player;
+
+	// fonction pour annoncer la victoire
+
+	var Winner = function(a)
+	{
+		if  (!bool)
+		{
+			$('#youWin').append('<div class="alert alert-success" role="alert" id="alert">');
+			if  (player === 'X')
+			{
+				$('#alert').append('<h1>Player 2 win!</h1>');
+			}
+			else 
+			{
+				$('#alert').append('<h1>Player 1 win!</h1>');
+			}
+		}
+	};
 
 	// fonction pour connaitre le player
 
 	$('td').click(function()
 	{
-		count++;
-		count % 2 === 0 ?	player = 'X' : player = 'O'; 		
+		var check = $(this).html();
+		if (bool && check === "")
+		{
+			count++;
+			count % 2 === 0 ?	player = 'X' : player = 'O'; 		
+		}
 	});
 
 	// fonction pour remplir le morpion au click
@@ -24,14 +46,16 @@ $(document).ready(function()
 
 	$('td').on('click', function()
 	{
-
-		var row = $(this).parent().attr('value');
-		var cell = $(this).attr('value');
-
-		if  (array[row][cell] === 1)
+		if  (bool)
 		{
-			array[row][cell] = player;
-			$(this).html(player);
+			var row = $(this).parent().attr('value');
+			var cell = $(this).attr('value');
+
+			if  (array[row][cell] === 1)
+			{
+				array[row][cell] = player;
+				$(this).html(player);
+			}
 		}
 		
 	});
@@ -40,21 +64,24 @@ $(document).ready(function()
 
 	$('td').on('click', function()
 	{
-		for  (var i = 0; i <= 2; i++)
+		if (bool)
 		{
-			var checkWin = "";
-			for  (var j = 0; j <= 2; j++)
+			for  (var i = 0; i <= 2; i++)
 			{
-				checkWin += array[i][j];
-				if  (checkWin.includes('XXX'))
+				var checkWin = "";
+				for  (var j = 0; j <= 2; j++)
 				{
-					$('#alert').append('<h1>Player 2 Win!</h1>');
-					break;
-				}
-				else if  (checkWin.includes('OOO'))
-				{
-					$('#alert').append('<h1>Player 1 Win!</h1>');
-					break;
+					checkWin += array[i][j];
+					if  (checkWin.includes('XXX'))
+					{
+						bool = false;
+						Winner(bool);
+					}
+					else if  (checkWin.includes('OOO'))
+					{
+						bool = false;
+						Winner(bool);
+					}
 				}
 			}
 		}
@@ -64,21 +91,24 @@ $(document).ready(function()
 
 	$('td').on('click', function()
 	{
-		for  (var i = 0; i <= 2; i++ )
+		if  (bool)
 		{
-			var checkWin = "";
-			for  (var j = 0; j <= 2; j++)
+			for  (var i = 0; i <= 2; i++ )
 			{
-				checkWin += array[j][i];
-				if  (checkWin.includes('XXX'))
+				var checkWin = "";
+				for  (var j = 0; j <= 2; j++)
 				{
-					$('#alert').append('<h1>Player 2 Win!</h1>');
-					return;
-				}
-				else if  (checkWin.includes('OOO'))
-				{
-					$('#alert').append('<h1>Player 1 Win!</h1>');
-					return;
+					checkWin += array[j][i];
+					if  (checkWin.includes('XXX'))
+					{
+						bool = false;
+						Winner(bool);
+					}
+					else if  (checkWin.includes('OOO'))
+					{
+						bool = false;
+						Winner(bool);
+					}
 				}
 			}
 		}
@@ -89,21 +119,35 @@ $(document).ready(function()
 
 	 $('td').on('click', function()
 	 {
-	 	var diagonale1 = [array[0][0], array[1][1], array[2][2]].join("");
-	 	var diagonale2 = [array[2][0], array[1][1], array[0][2]].join("");
-	 	
-	 	if  (diagonale1.includes('XXX') || diagonale2.includes('XXX'))
+	 	if  (bool)
 	 	{
-	 		$('#alert').append('<h1>Player 2 Win!</h1>');
-	 		return;
-	 	}
-	 	else if  (diagonale1.includes('OOO') || diagonale2.includes('OOO'))
-	 	{
-	 		$('#alert').append('<h1>Player 1 Win!</h1>');
-	 		return;
-	 	}
+		 	var diagonale1 = [array[0][0], array[1][1], array[2][2]].join("");
+		 	var diagonale2 = [array[2][0], array[1][1], array[0][2]].join("");
+		 	
+		 	if  (diagonale1.includes('XXX') || diagonale2.includes('XXX'))
+		 	{
+		 		bool = false;
+		 		Winner(bool);
+		 	}
+		 	else if  (diagonale1.includes('OOO') || diagonale2.includes('OOO'))
+		 	{
+		 		bool = false;
+		 		Winner(bool);
+		 	}
+		 }
 	 });
+	 
 
+	 $('#reset').on('click',function()
+	 {
+	 	count = 0;
+	 	array = [ 	[1, 1, 1],
+					[1, 1, 1],
+					[1, 1, 1],	];
+		bool = true;
+	 	$("td").html("");
+	 	$('#alert').remove();
+	 });
 	
 
 });
